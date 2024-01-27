42153b23bb6e291c8afcb6178eae920f9ad7a62b
tabcodes <tabcodes@tabcodes.dev>
2024-01-26 23:31:05 -0500

    Update vite configuration for dev env

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
     plugins: [
+        basicSsl(),
         laravel({
             input: [
                 'resources/sass/app.scss',
@@ -11,4 +13,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        https: true,
+    },
 });
